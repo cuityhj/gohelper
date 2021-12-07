717c50c43df726f615431c3cb953cb6f020c7238
yuhaijiao <yuhj@linkingthing.com>
2021-12-07 13:23:37 +0000

    add mac-48 to uint64

---
+++ hwaddr/hwaddr.go
@@ -0,0 +1,8 @@
+package hwaddr
+
+import "net"
+
+func Mac48ToUint64(mac net.HardwareAddr) uint64 {
+	return (uint64(mac[0]) | uint64(mac[1])<<8 | uint64(mac[2])<<16 | uint64(mac[3])<<24 |
+		uint64(mac[4])<<32 | uint64(mac[5])<<40)
+}
