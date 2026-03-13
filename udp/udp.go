578aae37559d35ef5460985380c171b71e971c6b
yuhaijiao <yuhj@linkingthing.com>
2026-03-13 03:37:13 +0000

    fix udp exchange return wrong repsonse

--- udp/udp.go
+++ udp/udp.go
@@ -22,26 +22,25 @@ func NewUDPClient(timeout time.Duration) *UDPClient {
 	return &UDPClient{timeout: timeout}
 }
 
-func (cli *UDPClient) Exchange(request []byte, serverIp net.IP, serverPort uint32, response []byte) error {
+func (cli *UDPClient) Exchange(request []byte, serverIp net.IP, serverPort uint32, response []byte) ([]byte, error) {
 	conn, err := net.DialUDP("udp", nil, &net.UDPAddr{IP: serverIp, Port: int(serverPort)})
 	if err != nil {
-		return fmt.Errorf("dial udp with addr %s and port %d failed: %s", serverIp, serverPort, err.Error())
+		return nil, fmt.Errorf("dial udp with addr %s and port %d failed: %s", serverIp, serverPort, err.Error())
 	}
 
 	defer conn.Close()
 
 	if _, err := conn.Write(request); err != nil {
-		return fmt.Errorf("write udp request to addr %s and port %d failed: %s", serverIp, serverPort, err.Error())
+		return nil, fmt.Errorf("write udp request to addr %s and port %d failed: %s", serverIp, serverPort, err.Error())
 	}
 
 	conn.SetReadDeadline(time.Now().Add(cli.timeout))
 	n, err := conn.Read(response)
 	if err != nil {
-		return fmt.Errorf("read udp respsonse from addr %s and port %d failed: %s", serverIp, serverPort, err.Error())
+		return nil, fmt.Errorf("read udp respsonse from addr %s and port %d failed: %s", serverIp, serverPort, err.Error())
 	}
 
-	response = response[:n]
-	return nil
+	return response[:n], nil
 }
 
 func (cli *UDPClient) Write(request []byte, serverIp net.IP, serverPort uint32) error {
